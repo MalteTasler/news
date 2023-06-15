3de60ec9ca41c542e58bd3344f1653e542ae885e
Malte T <m.d.tasler@gmail.com>
2023-06-15 11:03:59 +0200

    :wrench: add toolkit.config

---
+++ toolkit.config.js
@@ -0,0 +1,8 @@
+/* module.exports = {
+    development: {
+        host: process.env.DEV_HOST || '0.0.0.0',
+        port: parseInt(process.env.DEV_PORT, 10) || 8080,
+        cert: '//fs1/ssl/tobitag.crt',
+        key: '//fs1/ssl/tobitag.key',
+    },
+} */
